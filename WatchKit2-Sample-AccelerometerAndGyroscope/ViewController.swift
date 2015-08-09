cb5dfe38a91615260e0eb98b82205ed719a123e8
XuQing1001 <xq2417@gmail.com>
2015-08-09 14:32:39 +0800

    Initial Commit

---
+++ WatchKit2-Sample-AccelerometerAndGyroscope/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchKit2-Sample-AccelerometerAndGyroscope
+//
+//  Created by XuQing on 15/8/9.
+//  Copyright © 2015年 XuQing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
